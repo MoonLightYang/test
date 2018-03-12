f35d1c190106495b6d9be3009dc4cc9aae7543f0
liang.yang <562096674@qq.com>
2018-03-12 20:26:29 +0800

    你好

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,8 +4,10 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("123");
+		int c = 1;
 		System.out.println("123");
 		int a = 1;
+		int e = 1;
 		int b = 2;
 	}
 }
