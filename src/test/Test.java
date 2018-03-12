05714f8fee75c5ec49ed490c19d2993edb6994a3
MoonLightYang <729355767@qq.com>
2018-03-12 20:28:19 +0800

    123

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,8 +4,10 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("123");
+		System.out.println("1");
 		System.out.println("123");
 		int a = 1;
 		int b = 2;
+		System.out.println("1");
 	}
 }
